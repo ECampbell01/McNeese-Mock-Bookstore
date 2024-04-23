e92c8bde957d769a1271b174feebdc20d55a090c
Jett Rogers <35665999+JettRog5026@users.noreply.github.com>
2024-04-22 21:54:35 -0500

    Only post problem left

--- McNeese_Bookstore/deleteOffSup.php
+++ McNeese_Bookstore/deleteOffSup.php
@@ -7,20 +7,26 @@
     if(isset($_POST['productId'])){
 
         $deleteId = $_POST['productId'];
+        $customerId = $_POST['customerId'];
 
         // Query to get cart information
-        $query = "DELETE TOP(1) FROM cart WHERE `cart`.`SupplyId` = ?";
+        $query = "DELETE TOP(1) FROM cart WHERE `cart`.`SupplyId` = ? AND 'cart'.'CustomerId' = ?";
+
         $statement = $conn->prepare($query);
-        $statement->bind_param("i", $deleteId);
-        echo"hello";
+        $statement->bind_param("ii", $deleteId, $customerId);
+
         if ($statement->execute()) {
             // Item successfully removed
             echo "<script>alert('Item successfully removed.');</script>";
-            include("index.php");
+            include("cart.php");
         } else {
             // Error removing Item
             echo "<script>alert('Error removing item. Please try again.');</script>";
         }
 
+    }
+    else {
+        // Supply id is not provided
+        echo "<script>alert('Supply ID is missing.');</script>";
     }
 ?> 
